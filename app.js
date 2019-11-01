2f489604724281c9fa31c1c3078d20dd7b8bd5e3
haoxian0114 <haoxian0114@yahoo.com>
2019-11-01 20:12:22 +0000

    add app.js

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function(){
+//app.listen(3000, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
 	console.log("Yelp App has started!!!");
 });
